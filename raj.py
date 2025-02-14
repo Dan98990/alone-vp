91172b5f05843724087b0373b35cf4a6c36bbdf2
Dan98990 <coc68822@gmail.com>
2025-02-14 20:46:22 +0530

    Update raj.py

--- raj.py
+++ raj.py
@@ -174,7 +174,7 @@ async def run_attack(chat_id, ip, port, time, context):
 
     try:
         process = await asyncio.create_subprocess_shell(
-            f"./Alone {ip} {port} {time} 900",
+            f"./ALONE {ip} {port} {time} 900",
             stdout=asyncio.subprocess.PIPE,
             stderr=asyncio.subprocess.PIPE
         )
